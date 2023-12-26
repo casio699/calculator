8d65a6715bdad54fb4f9da70b9f8bc59d4203ad1
ahmad khoder <ahmad.khoder2332@gmail.com>
2023-12-26 04:06:23 +0200

    buttons algorithms and displays all functioning this took way too long because of i increment issues and mistakes

--- script.js
+++ script.js
@@ -1,32 +1,62 @@
 //declaring and initializing variables
-let toDisplay = 0;
-let memory1 = 0;
-//math functions:
+let toDisplay = '0';
+let memory = [];
+let op= null;
+let resultor= null;
+let i=1;
+
+// math functions:
 let add = (var1, var2) => var1+var2;
 let subtract = (var1, var2) => var1-var2;
 let multiply = (var1, var2) => var1*var2;
 let divide = (var1, var2) => var1/var2;
 
-// operate function:
+// operator functions:
 let operate = (var1,var2,op) => {
-    if      (op==='+')   {add(var1,var2)}
-    else if (op==='-')   {subtract(var1,var2)}
-    else if (op==='*')   {multiply(var1,var2)}
-    else if (op==='/')   {divide(var1,var2)}
+    if      (op==='+')   return add(var1,var2);
+    else if (op==='-')   return subtract(var1,var2);
+    else if (op==='*')   return multiply(var1,var2);
+    else if (op==='/')   return divide(var1,var2);
 }
 
-// cicking on number buttons
-const numberButtonClicked = document.querySelector(".numberButtons");
-numberButtonClicked.addEventListener("click", event=>{
+
+
+
+
+
+
+// clicking on number buttons
+const numbersClicked = document.querySelector(".numberButtons");
+numbersClicked.addEventListener("click", event=>{
     const displayedNb= event.target.closest("button");
-    toDisplay=displayedNb.textContent;
-    console.log("toDisplay: "+toDisplay);
-    const changedisplay=document.getElementById("display");
-    changedisplay.innerText+=toDisplay;
-    memory1=parseFloat(changedisplay.innerText);//storing the value
-    console.log("memory1: "+memory1);
+    toDisplay += displayedNb.textContent;
+    updateDisplay(toDisplay,i);
 });
 
 
 //clicking on operator buttons
-const operatorButtonClicked = document.querySelector(".opButtons")
+const operatorsClicked = document.querySelector(".opButtons")
+operatorsClicked.addEventListener("click", event=>{
+    op = event.target.closest("button").textContent; 
+    memory[i]=parseFloat(toDisplay);//storing the value of a number
+    updateDisplay(op,i);
+    toDisplay='';
+});
+
+//clicking on resultors
+const resultorsClicked = document.querySelector(".resultors")
+resultorsClicked.addEventListener("click", event => {
+    resultor = event.target.closest("button").textContent;
+    if(resultor==="="){
+        memory[++i]= parseFloat(toDisplay);
+        toDisplay=operate(memory[i-1],memory[i],op).toString();
+    }
+    else return updateDisplay(toDisplay='0',i=0); //clear display and reset variables
+    updateDisplay(toDisplay,i);
+});
+
+
+//displaying function
+let updateDisplay = (toDisplay,i) => {
+    document.getElementById("display").innerText=toDisplay;   
+    };
